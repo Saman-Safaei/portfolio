4cd9b605c8b8c717b144c17519a9643585634e9a
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-22 21:25:39 +0430

    create status component for index page

--- components/Index/Status.vue
+++ components/Index/Status.vue
@@ -1,3 +1,18 @@
 <template>
-  <div dir="rtl"></div>
+  <div dir="rtl" class="grid grid-cols-1 sm:grid-cols-3 gap-y-16">
+    <div class="text-center flex flex-col items-stretch gap-2">
+      <p class="text-4xl">2</p>
+      <h4 class="font-normal">پروژه های فعال</h4>
+    </div>
+
+    <div class="text-center flex flex-col items-stretch gap-2">
+      <p class="text-4xl">2</p>
+      <h4 class="font-normal">پروژه های موفق</h4>
+    </div>
+
+    <div class="text-center flex flex-col items-stretch gap-2">
+      <p class="text-4xl">0</p>
+      <h4 class="font-normal">درخواست های کاری</h4>
+    </div>
+  </div>
 </template>
