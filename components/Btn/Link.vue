fddfde804d492a4942ad5644c858423a8cd0a919
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-22 22:52:39 +0430

    make links better

--- components/Btn/Link.vue
+++ components/Btn/Link.vue
@@ -1,7 +1,10 @@
 <template>
-  <nuxt-link class="text-emerald-600 dark:text-emerald-200" :to="to"
-    >go to info</nuxt-link
-  >
+  <nuxt-link
+    class="hover:text-emerald-600 dark:hover:text-emerald-400 transition duration-300"
+    exact-active-class="text-emerald-600 dark:text-emerald-400"
+    :to="to">
+    <slot></slot>
+  </nuxt-link>
 </template>
 
 <script>
