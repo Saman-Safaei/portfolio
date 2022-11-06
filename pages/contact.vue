cb8c1ff41b303a3ce576622ad9e49ea71613350a
Saman-Safaei <xsamansafaeix@gmail.com>
2022-11-06 18:53:58 +0330

    update contact list info

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,10 +6,10 @@
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             <contact-info-input text="09196043787" title="شماره تماس" />
             <contact-info-input text="xsamansafaeix@gmail.com" title="ایمیل" />
-            <contact-info-input text="SamuelSam-dev" title="گیت هاب" />
-            <contact-info-input text="SamuelSam_dev" title="تلگرام" />
+            <contact-info-input text="Saman-Safaei" title="گیت هاب" />
+            <contact-info-input text="samansafaei_ir" title="تلگرام" />
             <contact-info-input text="hallowed_programmer" title="اینستاگرام" />
-            <contact-info-input text="09029226521" title="واتساپ" />
+            <contact-info-input text="09196043787" title="واتساپ" />
           </div>
         </ContactGlassCard>
       </Container>
